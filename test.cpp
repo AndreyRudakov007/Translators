99eef543e4a785747fad782a39209a557ee86d40
Andrey Rudakov <dron.lenovo@gmail.com>
2015-05-18 22:20:19 +0400

    AddFactorial

--- test.cpp
+++ test.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 double a = 5;
 double b = 6;
+int andr = 45;
+
+int GetFactorial(int res)
+{
+    return res==0 ? 1 : res * GetFactorial(res-1);
+}
+
+
 int main(){
 if ((a==b)) {
 printf("%lf", (a+b));
@@ -11,3 +19,6 @@ printf("%lf", (a+b));
 printf("%lf", (a-b));
 }
 }
+
+
+
